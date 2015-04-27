598ee59f7ab1e07c21ae7a3cd4b0d24e829354fe
David Tolpin <david.tolpin@gmail.com>
2015-04-27 11:51:10 +0100

    depends on dtolpin

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-gorilla "0.3.5-SNAPSHOT"
+(defproject dtolpin/lein-gorilla "0.3.5-SNAPSHOT"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
-  :dependencies [[gorilla-repl "0.3.5-SNAPSHOT"]]
+  :dependencies [[dtolpin/gorilla-repl "0.3.5-SNAPSHOT"]]
   :eval-in-leiningen true)
